9f7fa77c27d7e443c20caecea0e74eded9b53ae5
jgsheppa <james.sheppard@avisio.ai>
2021-09-29 23:31:49 +0200

    Add exit statuses

--- file_type.sh
+++ file_type.sh
@@ -3,16 +3,22 @@
 # This loops through the arguments given to the script
 for FILE in $@ 
 do
+  if [ "$FILE" = "file_type.sh" ]
+  then 
+    echo "This script is a file."
   # This checks if the chosen data is a file
-  if [ -f "$FILE" ] 
+  elif [ -f "$FILE" ] 
   then
     echo "$FILE is a file"
-    # This checks if the chosen data is a directory
+    exit 0
+  # This checks if the chosen data is a directory
   elif [ -d "$FILE" ] 
   then
     echo "$FILE is a directory"
+    exit 1
   else 
     echo "$FILE is neither a file or directory"
+    exit 2
   fi
   
   ls -l $FILE
--- shadow.sh
+++ shadow.sh
@@ -5,6 +5,8 @@ FILE="/etc/shadow"
 if [ -e "$FILE" ]
 then
     echo "You have permissions to edit /etc/shadow"
+    exit 0
 else
     echo "You do not have permissions to edit /etc/shadow"
+    exit 1
 fi
